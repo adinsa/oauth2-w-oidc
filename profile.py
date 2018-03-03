168e7ea177fa887b77b74fa676b88b61ef904ede
Amar Dinsa <dinsaa@gmail.com>
2018-03-03 14:50:40 -0500

    Fix name of node variable

--- profile.py
+++ profile.py
@@ -13,11 +13,10 @@ pc = portal.Context()
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
 
-# Node node1
-node1 = request.XenVM('node1')
-node1.routable_control_ip = True
-node1.disk_image = 'urn:publicid:IDN+apt.emulab.net+image+emulab-ops//UBUNTU14-10-64-STD'
-node1.Site('28')
+node = request.XenVM('node1')
+node.routable_control_ip = True
+node.disk_image = 'urn:publicid:IDN+apt.emulab.net+image+emulab-ops//UBUNTU14-10-64-STD'
+node.Site('28')
 
 # Install and execute setup script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
